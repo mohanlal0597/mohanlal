442ea0f022d7f368460d5499ae4dd17ec1b03dc8
mohanlal0597 <31504994+mohanlal0597@users.noreply.github.com>
2017-11-16 14:23:21 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -1,1 +1,18 @@
-# mohanlal
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a>1)
+	{
+		printf("%d is positive",a);
+		}
+	if(a<-1)
+		{
+			printf("%d is negative",a);
+			}
+	 if(a==0)
+			{
+				printf("%d is zero",a);
+				}
+}
