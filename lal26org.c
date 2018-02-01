ee6235ce36a0413e21df4e0798874c9cb92a74d9
mohanlal0597 <31504994+mohanlal0597@users.noreply.github.com>
2018-02-01 14:44:13 +0530

    Create lal26org.c

---
+++ lal26org.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,j,a[100],n,t;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
+return 0;
+}
