cf45165050a5b970bf9cf6f155587350473bd16a
mohanlal0597 <31504994+mohanlal0597@users.noreply.github.com>
2018-02-22 16:03:16 +0530

    Create lal49.c

---
+++ lal49.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the numbers:\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("sum of two number is:%d",a+b);
+    return 0;
+}
